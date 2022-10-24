7076a5a8057ca1429f109ca2709e5b0157ec6e10
Nicholas Lediet <nlediet@gmail.com>
2022-10-24 01:12:08 -0400

    clean up files and add some comments for next steps

--- src/pinterest.ts
+++ src/pinterest.ts
@@ -10,30 +10,35 @@ async function main() {
     waitForElment('[aria-label="More ideas"]')
         .then(element => {
             console.log('test')
+            // Validate we were able to select an element
             if(element == null) {
                 return;
             }
-    
+            
             const parentNode: Element = element.parentElement!;
-            console.log(parentNode);
             const newNode = parentNode.cloneNode(true) as Element;
 
             const getLastNodeResursively = (node: Element): Element => {
                 if(node.children.length === 0 ) {
-                    console.log('The last node: ' + node);
                     return node;
                 }
 
-                console.log('There is more to come')
                 return getLastNodeResursively(node.children[node.children.length -1 ])
             }
             let lastNode = getLastNodeResursively(newNode);
             lastNode.textContent = 'Shuffle board'
             
-            console.log(newNode);
-            console.log(parentNode.parentElement);
-
             parentNode.parentElement!.append(newNode);
+
+            // Clear all event listeners on children
+
+            // Setup a event listener on the button/current node or wherever idk.  maybe console log it out?
+
+            // Pinterest API time 
+                // Get the board
+                // Randomize the elements.
+                // Update the pinterest board
+
         });
 
     console.timeEnd();
